c5f3026e46e134a4cebc0437cd44f0d93dfacf86
yukson <youn9249@naver.com>
2024-08-14 16:47:49 +0900

    day14 react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+          <div className="w3-row-padding w3-padding-16 w3-center" id="food">
+            <div className="w3-quarter">
+            <img src="/w3images/sandwich.jpg" alt="Sandwich" style={{width:"100%"}} />
+            <h3>The Perfect Sandwich, A Real NYC classNameic</h3>
+            <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-quarter">
+            <img src="/w3images/steak.jpg" alt="Steak" style={{width:"100%"}} />
+            <h3>Let Me Tell You About This Steak</h3>
+            <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div classNameName="w3-quarter">
+            <img src="/w3images/cherries.jpg" alt="Cherries" style={{width:"100%"}} />
+            <h3>Cherries, interrupted</h3>
+            <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            <p>What else?</p>
+            </div>
+            <div className="w3-quarter">
+            <img src="/w3images/wine.jpg" alt="Pasta and Wine" style={{width:"100%"}} />
+            <h3>Once Again, Robust Wine and Vegetable Pasta</h3>
+            <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-row-padding w3-padding-16 w3-center">
+            <div className="w3-quarter">
+            <img src="/w3images/popsicle.jpg" alt="Popsicle" style={{width:"100%"}} />
+            <h3>All I Need Is a Popsicle</h3>
+            <p>Lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-quarter">
+            <img src="/w3images/salmon.jpg" alt="Salmon" style={{width:"100%"}} />
+            <h3>Salmon For Your Skin</h3>
+            <p>Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-quarter">
+            <img src="/w3images/sandwich.jpg" alt="Sandwich" style={{width:"100%"}} />
+            <h3>The Perfect Sandwich, A Real classNameic</h3>
+            <p>Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+            <div className="w3-quarter">
+            <img src="/w3images/croissant.jpg" alt="Croissant" style={{width:"100%"}} />
+            <h3>Le French</h3>
+            <p>Lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum.</p>
+            </div>
+        </div>
+        </div>);
 }
-
 export default App;
